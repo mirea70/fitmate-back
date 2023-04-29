e5e17dd709ad559c060b943b98c30f05611f8d7d
GrowMyself <deae670@daum.net>
2023-04-30 03:18:04 +0900

    Initial Commit

---
+++ src/main/java/com/fitmate/FitmateApplication.java
@@ -0,0 +1,13 @@
+package com.fitmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitmateApplication.class, args);
+	}
+
+}
