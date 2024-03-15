861e34e2c7d69223932ec7eedf7e3272e4d84924
mirea70 <deae670@daum.net>
2024-03-15 09:50:46 +0900

    test: github webhook 테스트

--- modules/domain/domain-rdbms/src/test/java/com/fitmate/domain/file/repository/AttachFileRepositoryTest.java
+++ modules/domain/domain-rdbms/src/test/java/com/fitmate/domain/file/repository/AttachFileRepositoryTest.java
@@ -15,6 +15,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @Import(AttachFileDomainTestHelper.class)
+/**
+  수정 필요 (테스트)
+**/
 public class AttachFileRepositoryTest {
     @Autowired
     private AttachFileRepository attachFileRepository;
