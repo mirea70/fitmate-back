24cb10fbc714d91390ee346179b7380a0e1551c1
GrowMyself <deae670@daum.net>
2024-03-23 11:22:29 +0900

    feat: 문자 전송 API 추가 - SMS 모듈 추가 - 통신사 점검 후, 재테스트 필요 - yml 이동 작업 필요(클라우드)

---
+++ modules/system/system-sms/src/test/java/com/fitmate/system/BeanScanner.java
@@ -0,0 +1,9 @@
+package com.fitmate.system;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanScanner {
+    public void contextLoads() {}
+}
---
+++ modules/system/system-sms/src/test/java/com/fitmate/system/SmsTest.java
@@ -0,0 +1,27 @@
+package com.fitmate.system;
+
+import com.fitmate.system.util.SmsUtil;
+import net.nurigo.sdk.NurigoApp;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("sms")
+public class SmsTest {
+
+    @Autowired
+    private SmsUtil smsUtil;
+
+    @Test
+    public void sendOneTest() {
+
+        String to = "01020601122";
+        String content = "sms 테스트";
+
+        smsUtil.sendOne(to, content);
+    }
+}
