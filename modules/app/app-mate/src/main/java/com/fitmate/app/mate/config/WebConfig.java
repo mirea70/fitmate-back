89cac85d7397b51e6118c92d88c8a5b8e39a4a0e
GrowMyself <deae670@daum.net>
2024-04-06 16:43:22 +0900

    fix: Forward-proxy 설정 (테스트중) Off - yml파일과 충돌 의심

--- modules/app/app-mate/src/main/java/com/fitmate/app/mate/config/WebConfig.java
+++ modules/app/app-mate/src/main/java/com/fitmate/app/mate/config/WebConfig.java
@@ -4,10 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
-@Configuration
-public class WebConfig {
-    @Bean
-    ForwardedHeaderFilter forwardedHeaderFilter() {
-        return new ForwardedHeaderFilter();
-    }
-}
+//@Configuration
+//public class WebConfig {
+//    @Bean
+//    ForwardedHeaderFilter forwardedHeaderFilter() {
+//        return new ForwardedHeaderFilter();
+//    }
+//}
