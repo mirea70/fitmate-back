5cf999835838bbdc5f788cbd458c2a86810626f2
mirea70 <deae670@daum.net>
2024-05-16 05:33:04 +0900

    refactor: sms 인증 API swagger 설명 변경

--- modules/adapter/src/main/java/com/fitmate/adapter/in/web/sms/controller/SmsController.java
+++ modules/adapter/src/main/java/com/fitmate/adapter/in/web/sms/controller/SmsController.java
@@ -20,14 +20,18 @@ import javax.validation.Valid;
 public class SmsController {
     private final SmsUseCasePort smsUseCasePort;
 
-    @Operation(summary = "문자 인증 요청", description = "문자 인증 요청 API (유효기간 60초) \n\n **-> 현재 실제 문자 요청은 요금 부담으로 인해 차단해놓았습니다.**")
+    @Operation(summary = "문자 인증 요청", description = """
+            문자 인증 요청 API (유효기간 60초) : 토큰 필요 X
+            
+            **-> 현재 실제 문자 요청은 요금 부담으로 인해 차단해놓았습니다.**
+            """)
     @PostMapping("/request/code")
     public ResponseEntity<?> requestValidateCode(@Valid @RequestBody SmsCodeRequest request) {
         smsUseCasePort.requestValidateCode(request.getPhone());
         return ResponseEntity.ok().build();
     }
 
-    @Operation(summary = "인증번호 체크", description = "인증번호 체크 API")
+    @Operation(summary = "인증번호 체크", description = "인증번호 체크 API : 토큰 필요 X")
     @GetMapping("/check/code")
     public ResponseEntity<?> checkValidateCode(@Parameter(description = "사용자 입력값 (인증번호)")
                                                @RequestParam String inputCode) {
