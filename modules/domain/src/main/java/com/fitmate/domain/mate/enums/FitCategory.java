7facea5e10be9e1865d81fede15aec4b2134bfb5
mirea70 <deae670@daum.net>
2026-04-03 18:02:51 +0900

    refactor: 운동 카테고리 종류 추가

--- modules/domain/src/main/java/com/fitmate/domain/mate/enums/FitCategory.java
+++ modules/domain/src/main/java/com/fitmate/domain/mate/enums/FitCategory.java
@@ -8,6 +8,17 @@ import lombok.RequiredArgsConstructor;
 public enum FitCategory {
     FITNESS("헬스"),
     CROSSFIT("크로스핏"),
+    RUNNING("러닝"),
+    PILATES("필라테스"),
+    YOGA("요가"),
+    SWIMMING("수영"),
+    BADMINTON("배드민턴"),
+    TENNIS("테니스"),
+    CLIMBING("클라이밍"),
+    SOCCER("축구/풋살"),
+    BASKETBALL("농구"),
+    BASEBALL("야구"),
+    CYCLING("자전거"),
     ETC("기타"),
     ;
 
