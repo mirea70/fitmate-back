e0c5682b9591a060035c4ed9050b3a4670288382
mirea70 <deae670@daum.net>
2026-04-09 21:04:52 +0900

    refactor: Async 스레드 관련 설정 추가, 메인 클래스에 존재하던 설정들 별도 파일로 분리

---
+++ modules/adapter/src/main/java/com/fitmate/adapter/in/web/config/RetryConfig.java
@@ -0,0 +1,9 @@
+package com.fitmate.adapter.in.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@Configuration
+@EnableRetry
+public class RetryConfig {
+}
---
+++ modules/adapter/src/main/java/com/fitmate/adapter/in/web/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.fitmate.adapter.in.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
